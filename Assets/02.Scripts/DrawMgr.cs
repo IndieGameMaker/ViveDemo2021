0791224db8b850920e04cf42a56a1d62a1eeb6cf
Lee Jae Hyun <myleje@gmail.com>
2021-05-25 11:02:04 +0900

    .

--- Assets/02.Scripts/DrawMgr.cs
+++ Assets/02.Scripts/DrawMgr.cs
@@ -9,6 +9,11 @@ public class DrawMgr : MonoBehaviour
     private SteamVR_Action_Boolean trigger;
     private SteamVR_Action_Pose pose;
 
+    public float lineWidth = 0.01f;
+    public Color lineColor = Color.white;
+
+    private LineRenderer line;
+
     void Start()
     {
         rightHand = SteamVR_Input_Sources.RightHand;        
@@ -16,9 +21,35 @@ public class DrawMgr : MonoBehaviour
         pose      = SteamVR_Actions.default_Pose;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (trigger.GetStateDown(rightHand))
+        {
+            CreateLineObject();
+        }        
+    }
+
+    void CreateLineObject()
+    {
+        GameObject lineObject = new GameObject("Line");
+        line = lineObject.AddComponent<LineRenderer>();
+
+        Material mt = new Material(Shader.Find("Unlit/Color"));
+        mt.color = lineColor;
+
+        line.material = mt;
+        line.useWorldSpace = false;
+
+        line.numCapVertices = 20;
+        line.numCornerVertices = 20;
+
+        line.widthMultiplier = 0.1f;
+        line.startWidth = lineWidth;
+        line.endWidth = lineWidth;
+
+        line.positionCount = 1;
+
+        Vector3 position = pose.GetLocalPosition(rightHand);
+        line.SetPosition(0, position);
     }
 }
